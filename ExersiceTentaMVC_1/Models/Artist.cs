1a0526b83165fe77df970c597bad6e54d5695fe8
Dev15Durgam <blackros_91@hotmail.com>
2016-05-23 09:55:02 +0200

    uppgift_1

---
+++ ExersiceTentaMVC_1/Models/Artist.cs
@@ -0,0 +1,17 @@
+﻿using ExersiceTentaMVC_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExersiceTentaMVC_1.Models
+{
+    class Artist
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Band { get; set; }
+        public List<Song> Songs { get; set; }
+    }
+}
---
+++ ExersiceTentaMVC_1/Models/Song.cs
@@ -0,0 +1,10 @@
+﻿namespace ExersiceTentaMVC_1.Models
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public decimal Duration { get; set; }
+        public string Genre { get; set; }
+
+    }
+}
